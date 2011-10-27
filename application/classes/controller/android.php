b8fe6ca9b15b41365190e86c3d14f265b87e32f1
Miika Ojamo <miika@vkoski.net>
2011-10-27 19:49:54 +0300

    jos ei kuitenkaan päästtäis sinne aivopieruja.

--- application/classes/controller/android.php
+++ application/classes/controller/android.php
@@ -271,10 +271,12 @@ class Controller_Android extends Controller{
                         $result2 = false;
 
                     $vaihtehdot = array();
-                    $vaihtoehdot[0] = "twitter";
+                    $vaihtoehdot[0]["id"] = 0;
+                    $vaihtoehdot[0]["text"] = "twitter";
                     if($result2) foreach($result2 as $row => $data){
-                        $vaihtoehdot[$row]["text"] = $this->utf8($data['tunniste']);
-                        $vaihtoehdot[$row]["id"] = $this->utf8($data['id']);
+                        $rivi = $row + 1;
+                        $vaihtoehdot[$rivi]["text"] = $this->utf8($data['tunniste']);
+                        $vaihtoehdot[$rivi]["id"] = $this->utf8($data['id']);
                     }
 
                     $return = array("ret"=>$vaihtoehdot);
