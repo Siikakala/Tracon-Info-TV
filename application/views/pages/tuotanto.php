70a1e09d3b58b7fb0172fb6ce49369fbaa1d995d
Miika Ojamo <miika@vkoski.net>
2012-06-17 00:45:39 +0300

    Tuotantosuunnitelman init.

---
+++ application/views/pages/tuotanto.php
@@ -0,0 +1,17 @@
+<br/><br/>
+<table class="stats">
+    <thead>
+        <tr>
+            <th class="ui-state-default">Tärkeys</th>
+            <th class="ui-state-default">Kategoria</th>
+            <th class="ui-state-default">Alkuaika</th>
+            <th class="ui-state-default">Kesto</th>
+            <th class="ui-state-default">Tapahtuma</th>
+            <th class="ui-state-default">Vastuuhenkilö</th>
+            <th class="ui-state-default">Tekijät</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php print $tablebody; ?>
+    </tbody>
+</table>
