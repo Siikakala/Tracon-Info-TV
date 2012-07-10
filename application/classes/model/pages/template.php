e704d0afcf99cc6252825e93858cc15ef8feedb5
Miika Ojamo <miika@vkoski.net>
2012-07-10 08:05:35 +0300

    Koodin uudelleensijoittelu: pohjatemplate

---
+++ application/classes/model/pages/template.php
@@ -0,0 +1,21 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ *
+ *
+ */
+class Model_Pages_ extends Model_Database {
+
+    public function access(){
+
+    }
+    public function page(){
+
+    }
+    public function ajax(){
+
+    }
+}
+
+
+?>
