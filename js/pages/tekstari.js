dd26fa05ce5dfb35ed8474e64d5a8a45ed9d5221
Miika Ojamo <miika@vkoski.net>
2013-09-10 00:49:24 +0300

    Jotain unohtu.

--- js/pages/tekstari.js
+++ js/pages/tekstari.js
@@ -30,6 +30,7 @@ $(function(){
                 }else{
                     inform($("#feedback"),responseJSON.ret);
                 }
+                update_progress();
             },
         });
 
