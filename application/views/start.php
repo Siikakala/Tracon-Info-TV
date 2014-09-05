5075dbbcb131282d5f52e08b27e75a88c4d0cade
Miika Ojamo <miika@vkoski.net>
2014-09-06 01:21:59 +0300

    drop twitter

--- application/views/start.php
+++ application/views/start.php
@@ -27,13 +27,6 @@ print"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";?>
     			<?php print $text; ?>
     		</div>
 		</div>
-		<div id="twitter" style="display:none;">
-    		<div id="twitter_cont" style="display:block;position:relative;margin: -30px auto 0 auto">
-                <a class="twitter-timeline" data-dnt="true" width="900" height="500" data-chrome="nofooter" href="https://twitter.com/search?q=%23tracon" data-widget-id="348032480055529472">Tweets about "#tracon"</a>
-                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
-            </div>
-        </div>
    		<div id="footer">
     		<p><span id="client"></span></p>
 		</div>
